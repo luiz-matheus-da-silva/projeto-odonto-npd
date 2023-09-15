3502c620521d813334cb3f93a6bd791ffdc24033
Luiz Matheus <luizmatheus.profissional@hotmail.com>
2023-09-15 16:56:50 -0300

    Tela Principal

---
+++ odonto-csharp/odonto-csharp/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace odonto_csharp
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+            this.panelside = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtDataHora = new System.Windows.Forms.Label();
+            this.btnPacientes = new System.Windows.Forms.Button();
+            this.btnNovoCadastro = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerAtualizacao = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panelside.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelside
+            // 
+            this.panelside.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(91)))), ((int)(((byte)(140)))));
+            this.panelside.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panelside.Controls.Add(this.panel1);
+            this.panelside.Controls.Add(this.btnPacientes);
+            this.panelside.Controls.Add(this.btnNovoCadastro);
+            this.panelside.Controls.Add(this.pictureBox1);
+            this.panelside.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelside.Location = new System.Drawing.Point(0, 0);
+            this.panelside.Name = "panelside";
+            this.panelside.Size = new System.Drawing.Size(200, 729);
+            this.panelside.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.txtDataHora);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 629);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtDataHora
+            // 
+            this.txtDataHora.AutoSize = true;
+            this.txtDataHora.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDataHora.ForeColor = System.Drawing.Color.White;
+            this.txtDataHora.Location = new System.Drawing.Point(35, 71);
+            this.txtDataHora.Name = "txtDataHora";
+            this.txtDataHora.Size = new System.Drawing.Size(53, 20);
+            this.txtDataHora.TabIndex = 3;
+            this.txtDataHora.Text = "label1";
+            this.txtDataHora.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPacientes
+            // 
+            this.btnPacientes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPacientes.FlatAppearance.BorderSize = 0;
+            this.btnPacientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPacientes.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPacientes.ForeColor = System.Drawing.Color.White;
+            this.btnPacientes.Image = ((System.Drawing.Image)(resources.GetObject("btnPacientes.Image")));
+            this.btnPacientes.Location = new System.Drawing.Point(-116, 203);
+            this.btnPacientes.Name = "btnPacientes";
+            this.btnPacientes.Size = new System.Drawing.Size(313, 36);
+            this.btnPacientes.TabIndex = 2;
+            this.btnPacientes.Text = "                               Pacientes";
+            this.btnPacientes.UseVisualStyleBackColor = true;
+            // 
+            // btnNovoCadastro
+            // 
+            this.btnNovoCadastro.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNovoCadastro.FlatAppearance.BorderSize = 0;
+            this.btnNovoCadastro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNovoCadastro.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovoCadastro.ForeColor = System.Drawing.Color.White;
+            this.btnNovoCadastro.Image = ((System.Drawing.Image)(resources.GetObject("btnNovoCadastro.Image")));
+            this.btnNovoCadastro.Location = new System.Drawing.Point(-116, 143);
+            this.btnNovoCadastro.Name = "btnNovoCadastro";
+            this.btnNovoCadastro.Size = new System.Drawing.Size(316, 36);
+            this.btnNovoCadastro.TabIndex = 1;
+            this.btnNovoCadastro.Text = "                                        Novo Cadastro";
+            this.btnNovoCadastro.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.pictureBox1.Size = new System.Drawing.Size(176, 165);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timerAtualizacao
+            // 
+            this.timerAtualizacao.Enabled = true;
+            this.timerAtualizacao.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(200, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.pictureBox2.Size = new System.Drawing.Size(1150, 729);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panelside);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmPrincipal";
+            this.Text = "Tela Inicial";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panelside.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelside;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNovoCadastro;
+        private System.Windows.Forms.Button btnPacientes;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label txtDataHora;
+        private System.Windows.Forms.Timer timerAtualizacao;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ odonto-csharp/odonto-csharp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odonto_csharp
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Configure o Timer para atualizar a cada segundo (1000 ms)
+            timerAtualizacao.Interval = 1000;
+            timerAtualizacao.Start();
+
+            // Chame a função de atualização imediatamente para definir a data e hora inicial
+            AtualizarDataHora();
+        }
+
+        private void timerAtualizacao_Tick(object sender, EventArgs e) {
+            // Ocorre a cada intervalo definido no Timer
+            AtualizarDataHora();
+        }
+
+        private void AtualizarDataHora() {
+            // Obtém a data e hora atuais do sistema
+            DateTime dataHoraAtual = DateTime.Now;
+
+            // Define o texto do rótulo para exibir a data e hora
+            txtDataHora.Text = dataHoraAtual.ToString("dd/MM/yyyy HH:mm");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            AtualizarDataHora();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e) {
+            AtualizarDataHora();
+        }
+    }
+}
